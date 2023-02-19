2708f5b6858cc6cda85749e11a9a73c2a4416f1c
RiP3rQ <rafalpompa2000@gmail.com>
2023-02-19 20:14:58 +0100

    Customers page

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -14,7 +14,12 @@ export const api = createApi({
       query: () => "client/products",
       providesTags: ["Products"],
     }),
+    getCustomers: build.query({
+      query: () => "client/customers",
+      providesTags: ["Customers"],
+    }),
   }),
 });
 
-export const { useGetUserQuery, useGetProductsQuery } = api;
+export const { useGetUserQuery, useGetProductsQuery, useGetCustomersQuery } =
+  api;
