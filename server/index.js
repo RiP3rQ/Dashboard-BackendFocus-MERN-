ed871cc06dd5274c240b3a4f5edd50d534fa355f
RiP3rQ <rafalpompa2000@gmail.com>
2023-02-19 22:18:12 +0100

    overview page

--- server/index.js
+++ server/index.js
@@ -15,11 +15,13 @@ import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
+import OverallStat from "./models/OverallStat.js";
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
+  dataOverallStat,
 } from "./data/index.js";
 
 /* CONFIGURATION */
@@ -55,5 +57,6 @@ mongoose
     // Done -- Product.insertMany(dataProduct);
     // Done -- ProductStat.insertMany(dataProductStat);
     // Done -- Transaction.insertMany(dataTransaction);
+    // Done -- OverallStat.insertMany(dataOverallStat);
   })
   .catch((error) => console.log(`${error} did not connect`));
