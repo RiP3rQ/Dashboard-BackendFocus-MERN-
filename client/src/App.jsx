0f3d3dedef9b2204d20be1d81b9710ada4b4a5ad
RiP3rQ <rafalpompa2000@gmail.com>
2023-02-19 15:03:24 +0100

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
-import { themeSettings } from "theme";
-import { useSelector } from "react-redux";
 import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { themeSettings } from "theme";
+
+import Layout from "scenes/layout";
+import Dashboard from "scenes/dashboard";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -11,9 +15,17 @@ function App() {
 
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/scenes/layout/index.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { Box, useMediaQuery } from "@mui/material";
+import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Navbar from "components/Navbar";
+import Sidebar from "components/Sidebar";
+import { useGetUserQuery } from "state/api";
+
+const Layout = () => {
+  const isNonMobile = useMediaQuery("(min-width: 600px)");
+  return (
+    <Box width="100%" height="100%">
+      <Navbar />
+      <Outlet />
+    </Box>
+  );
+};
+
+export default Layout;
